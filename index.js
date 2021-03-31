e974f2ee2b17cac599a3cf6bf18584b110c2cb58
Jens Kristoffersen <jensakrr@gmail.com>
2021-03-31 20:13:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const request = require("request");
+
+const PORT = 5050;
+const HOST = "localhost";
+const API_SERVICE_URL = "https://www.metaweather.com/api/";
+
+app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/search", (req, res) => {
+  const query = req.query["query"];
+
+  const url = API_SERVICE_URL + "location/search/?query=" + query;
+  console.log(url);
+
+  request(url).pipe(res);
+});
+
+app.get("/coords", (req, res) => {
+  const lattlong = req.query["lattlong"];
+
+  const url = API_SERVICE_URL + "location/search/?lattlong=" + lattlong;
+  console.log(url);
+
+  request(url).pipe(res);
+});
+
+app.get("/location", (req, res) => {
+  const woeid = req.query["woeid"];
+
+  const url = API_SERVICE_URL + "location/" + woeid;
+  console.log(url);
+
+  request(url).pipe(res);
+});
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
